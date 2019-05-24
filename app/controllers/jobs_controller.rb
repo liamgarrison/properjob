dd09a3f2d6837540b9e8bab1315b5574cb7245ea
Liam Garrison <liamgarrison@gmail.com>
2019-05-24 12:20:31 +0100

    add waiting fo logic to controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,12 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show]
+  before_action :set_job, only: [:waiting_for, :show]
 
   def index
     @jobs = policy_scope(Job).all
   end
 
   def show
+    @waiting_for = waiting_for
   end
 
   private
@@ -13,5 +14,28 @@ class JobsController < ApplicationController
   def set_job
     @job = policy_scope(Job).find(params[:id])
     authorize @job
+  end
+
+  def waiting_for
+    # Find out what stage of the job we are at
+    # For that stage, which user type are we waiting for
+    waiting_for_stages = {
+      1 => "landlord",
+      2 => "contractor",
+      3 => "landlord",
+      4 => "contractor",
+      5 => "tenant",
+      6 => "contractor",
+      7 => "tenant",
+      8 => "landlord",
+      9 => nil
+    }
+    return waiting_for_stages[@job.current_stage]
+  end
+
+  def waiting_for_me?
+    # Find out if the current user is the one we are waiting on.
+    # (If so, display the call to action button)
+    return current_user.user_type == waiting_for
   end
 end
