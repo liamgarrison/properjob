acf68e63c4778313f8df45dc0e3a46c79888970b
Tom Dove <dovet@live.com>
2019-05-24 17:18:42 +0100

    Completed step two of logic flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'jobs#index'
   resources :jobs, only: [:show, :new, :create, :edit, :update] do
-    resources :quotes, only: [:new, :create, :edit, :update, :index]
+    resources :quotes, only: [:create, :update]
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
