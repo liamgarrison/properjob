2a5c16b25bccb584d6f23573c829b650d718b9db
Liam Garrison <liamgarrison@gmail.com>
2019-05-28 15:44:24 +0100

    imported new stripe.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,6 @@
 import "bootstrap";
+import { initStripe } from "./plugins/initStripe"
+
+// Initialize plugins
+
+initStripe();
---
+++ app/javascript/packs/plugins/initStripe.js
@@ -0,0 +1,7 @@
+const initStripe = () => {
+  const stripeScriptTag = document.getElementById('stripe-tag');
+  console.log(stripeScriptTag);
+  // const stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+}
+
+export { initStripe }
