ac67ab06a7e231dacedbbb9f7c9b70e7afebb7d0
Tom Dove <dovet@live.com>
2019-05-29 14:51:39 +0100

    .

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 gem 'stripe'
+gem 'ratyrate'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   root to: 'jobs#index'
   resources :jobs, only: [:show, :new, :create, :edit, :update] do
     resources :quotes, only: [:create, :update]
