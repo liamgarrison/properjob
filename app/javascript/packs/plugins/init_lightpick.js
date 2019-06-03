875509dfd2c966d59d39cd4e1cd38ae3638a0e46
Liam Garrison <liamgarrison@gmail.com>
2019-06-03 16:57:31 +0100

    fixed js bug in lightpick

--- app/javascript/packs/plugins/init_lightpick.js
+++ app/javascript/packs/plugins/init_lightpick.js
@@ -3,29 +3,28 @@ import Lightpick from 'lightpick'
 const initLightpick = () => {
   const timeSelectors = document.getElementById('time_selectors');
   const parentEl = document.querySelector('.popover-datepicker');
-  const contractorDates = JSON.parse(parentEl.dataset.contractorDates).map((date) => new Date(date))
-
-  const daysToShow = 91
-  const disableDates = []
-
-  let placeholderDate = new Date()
-  for (let i = 1; i <= daysToShow; i++) {
-    let isDisabled = true;
-    contractorDates.forEach((contractorDate) => {
-      console.log(contractorDate, placeholderDate)
-      if (contractorDate.toDateString() === placeholderDate.toDateString()) {
-        isDisabled = false;
-        console.log('enabling date')
-      }
-    })
-    if (isDisabled) {
-      disableDates.push(new Date(placeholderDate))
-    }
-    // Increment the date
-    placeholderDate.setDate(placeholderDate.getDate() + 1)
-  }
 
   if (parentEl && timeSelectors) {
+    const contractorDates = JSON.parse(parentEl.dataset.contractorDates).map((date) => new Date(date))
+    const daysToShow = 91
+    const disableDates = []
+
+    let placeholderDate = new Date()
+    for (let i = 1; i <= daysToShow; i++) {
+      let isDisabled = true;
+      contractorDates.forEach((contractorDate) => {
+        console.log(contractorDate, placeholderDate)
+        if (contractorDate.toDateString() === placeholderDate.toDateString()) {
+          isDisabled = false;
+        }
+      })
+      if (isDisabled) {
+        disableDates.push(new Date(placeholderDate))
+      }
+      // Increment the date
+      placeholderDate.setDate(placeholderDate.getDate() + 1)
+    }
+
     const picker = new Lightpick({
       field: timeSelectors,
       singleDate: true,
