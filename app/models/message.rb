84cab206aae8d76a072dc40bd7bc2a9c436c782d
Corrie Mosley <corrie.mosley@gmail.com>
2019-06-05 17:55:13 +0100

    messaging function added

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,8 @@ class Message < ApplicationRecord
   after_create :broadcast_message
 
   def broadcast_message
-    ActionCable.server.broadcast('messages', {
-      message_partial: ApplicationController.renderer.render(partial: "messages/message", locals: { message: self, belongs_to: 'theirs', message_owner_id: user.id }),
+    ActionCable.server.broadcast("messages_#{job.id}", {
+      message_partial: ApplicationController.renderer.render(partial: "messages/message", locals: { message: self, belongs_to: 'theirs' }),
       message_owner_id: user.id
     })
   end
