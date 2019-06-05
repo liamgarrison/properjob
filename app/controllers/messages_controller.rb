3784cc21fb3debd9afc098313bb6703361922d59
Liam Garrison <liamgarrison@gmail.com>
2019-06-05 10:57:14 +0100

    message styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,19 @@
 class MessagesController < ApplicationController
   def index
     @job = Job.find(params[:job_id])
+    @messages = Message.all
   end
 
   def create
-    raise
+    job = Job.find(params[:job_id])
+    message = Message.new(message_params)
+    message.user = current_user
+    message.job = job
+    message.save
+    redirect_to job_messages_path(job)
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :job)
   end
 end
